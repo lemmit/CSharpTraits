adfb1328e70fccd1c05dcb6ecddf5470abc64bf4
lemm <ten.michal.k@gmail.com>
2016-01-27 16:30:49 +0100

    Added example of how default interface implementation can be provided

---
+++ CSharpInterfaceWithDefaultImplementation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSharpInterfaceWithDefaultImplementation
+{
+    public interface ILogger
+    {
+        void Log(object @object);
+    }
+
+    public static class LoggerTrait
+    {
+        public static void Log(this ILogger logger, object @object, bool @default)
+        {
+            Console.WriteLine("Default: " + @object);
+        }
+    }
+
+
+    class DefaultLogger : ILogger
+    {
+        public void Log(object @object)
+        {
+            this.Log(@object, @default: true);
+        }
+    }
+
+    class RedefinedLogger : ILogger
+    {
+        public void Log(object @object)
+        {
+            Console.WriteLine("Redefinedlogger: " + @object);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new DefaultLogger();
+            logger.Log("1");
+
+            var redef = new RedefinedLogger();
+            redef.Log("2");
+
+            Console.ReadLine();
+        }
+    }
+}
+
